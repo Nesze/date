bb3fc5c9ec7c945183e300512ef282dd940e0705
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-07-09 21:37:11 +0300

    added graceful exit

--- date.go
+++ date.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+        "os"
+        "os/signal"
 	"time"
 )
 
 func main() {
+        ctrlc := make(chan os.Signal)
+        signal.Notify(ctrlc, os.Interrupt, os.Kill)
+
 	ticker := time.Tick(time.Second)
-	for {
+	go func() {
+            for {
 		<-ticker
 		fmt.Printf("\r%s", time.Now().Format(time.UnixDate))
-	}
+            }
+	}()
+
+        <-ctrlc
+        fmt.Println()
 }
