9b9f1f94ff49803bb932b7988f4a85290e333e4e
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-07-09 21:24:11 +0300

    init version

---
+++ date.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    ticker := time.Tick(time.Second)
+    for {
+        <- ticker
+        fmt.Printf("\r%s", time.Now().Format(time.UnixDate))
+    }
+}
